e5caa99ac6e7b7179781a3fe2ebd1923e356756b
Max Graey <maxgraey@gmail.com>
2020-01-20 19:35:28 +0200

    [rust] Rewrite Rust example (#3)

--- wasm_apps/rust/app.rs
+++ wasm_apps/rust/app.rs
@@ -3,29 +3,29 @@
 mod arduino_api;
 use arduino_api::*;
 
-static LED: u32 = 19;
-
-fn setup() {
-    pin_mode(LED, OUTPUT);
+struct App {
+  led: u32,
 }
 
-fn run() {
-    digital_write(LED, HIGH);
-    delay(100);
-    digital_write(LED, LOW);
-    delay(900);
-}
+impl App {
+    fn new() -> Self {
+        let led = get_pin_led();
+        pin_mode(led, OUTPUT);
+        Self { led }
+    }
 
-/*
- * Entry point
- */
+    fn update(&self) {
+        digital_write(self.led, HIGH);
+        delay(100);
+        digital_write(self.led, LOW);
+        delay(900);
+    }
+}
 
 #[no_mangle]
 pub extern fn _start() {
-    setup();
-    loop {
-        run();
-    }
+    let app = App::new();
+    loop { app.update() };
 }
 
 #[panic_handler]

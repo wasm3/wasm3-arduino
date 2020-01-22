1ba632360b3f46f2f14ed21d4e49bde2abe6e44e
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2020-01-22 03:01:11 +0200

    Remove prints for now

--- wasm_apps/assemblyscript/app.ts
+++ wasm_apps/assemblyscript/app.ts
@@ -4,14 +4,9 @@ let LED: u32 = dev.getPinLED();
 
 function setup(): void {
   dev.pinMode(LED, dev.OUTPUT);
-
-  dev.print('Hello from AssemblyScript 😊\n');
 }
 
 function run(): void {
-  const t: u32 = dev.millis();
-  dev.println('[' + t.toString() + '] ' + 'Тест юнікоду.');
-
   dev.digitalWrite(LED, dev.HIGH);
   dev.delay(100);
   dev.digitalWrite(LED, dev.LOW);

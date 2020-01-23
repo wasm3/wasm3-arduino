a6c90adf20ecbb14f96a3d8ad558efcffcbd79f2
MaxGraey <maxgraey@gmail.com>
2020-01-23 23:00:59 +0200

    refactor

--- examples_pio/Wasm_Advanced/wasm_apps/assemblyscript/arduino.ts
+++ examples_pio/Wasm_Advanced/wasm_apps/assemblyscript/arduino.ts
@@ -5,15 +5,17 @@ export const INPUT        = 0x0;
 export const OUTPUT       = 0x1;
 export const INPUT_PULLUP = 0x2;
 
-
 export declare function millis(): u32;
 export declare function delay(ms: u32): void;
 export declare function pinMode(pin: u32, mode: u32): void;
 export declare function digitalWrite(pin: u32, value: u32): void;
 export declare function getPinLED(): u32;
 
-@external("getGreeting")  declare function _getGreeting(ptr: usize, maxlen: usize): void;
-@external("print")        declare function _print(ptr: usize, len: usize): void;
+@external("getGreeting")
+declare function _getGreeting(ptr: usize, maxlen: usize): void;
+
+@external("print")
+declare function _print(ptr: usize, len: usize): void;
 
 export function print(str: string): void {
   const buff = String.UTF8.encode(str)
